973303afee91d0391d5ffed0f8d21ea3adef5f18
Fernando Yang <fernando38954@gmail.com>
2025-10-29 22:39:02 -0300

    feature: implementar logica da funcao de cifra de cesar

--- solucoes.py
+++ solucoes.py
@@ -3,8 +3,15 @@ def sao_anagramas(string1, string2):
     pass
 
 def cifra_de_cesar(texto, deslocamento):
-    # TODO: Implementar lógica
-    pass
+    cifra = ""
+    for char in texto:
+        if char.isalpha():
+            ascii_base = ord('A') if char.isupper() else ord('a')
+            nova_posicao = (ord(char) - ascii_base + deslocamento) % 26
+            cifra += chr(nova_posicao + ascii_base)
+        else:
+            cifra += char
+    return cifra
 
 def valida_cpf(cpf_string):
     # TODO: Implementar lógica
